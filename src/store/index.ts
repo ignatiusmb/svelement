52e809801397486dbdfdfae79a94dabc2871df04
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-05-10 23:15:09 +0700

    export storage store function

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
-import { writable } from 'svelte/store';
+import type { Writable } from 'svelte/store';
+import { get, writable } from 'svelte/store';
 
 const cache = new Map();
 
@@ -10,4 +11,37 @@ export function fetcher(url: RequestInfo) {
 		cache.set(url, data), store.set(Promise.resolve(data));
 	};
 	return load(), store;
+}
+
+export function storage<T>(
+	key: string,
+	value: T,
+	type: 'local' | 'session' = 'local'
+): Writable<T> {
+	const getStorage =
+		type === 'local'
+			? () => typeof localStorage !== 'undefined' && localStorage
+			: () => typeof sessionStorage !== 'undefined' && sessionStorage;
+
+	const ss = getStorage();
+	const initial = writable(value);
+	const updateStorage = (key: string, value: T) => {
+		const ss = getStorage();
+		if (ss) ss.setItem(key, JSON.stringify(value));
+	};
+
+	if (ss && ss[key]) initial.set(JSON.parse(ss[key]));
+
+	return {
+		subscribe: initial.subscribe,
+		set(value) {
+			initial.set(value);
+			updateStorage(key, value);
+		},
+		update(updater) {
+			const value = updater(get(initial));
+			initial.set(value);
+			updateStorage(key, value);
+		},
+	};
 }
